87f54dc5d32463d89f0f1b1c414952360ddab799
= <=>
2023-05-30 16:44:27 -0600

    Modified page Callback code

--- app/api/infojobs-callback/page.jsx
+++ app/api/infojobs-callback/page.jsx
@@ -7,11 +7,16 @@ export default function InfoJobsCallback() {
   const { code } = router.query // Obtiene el código de verificación de los parámetros de consulta
   console.log(code)
 
-  // Realiza cualquier procesamiento adicional necesario con el código de verificación
-  // Por ejemplo, puedes almacenar el código de verificación en una variable de estado o en una cookie
+  useEffect(() => {
+    // Verifica si el código de verificación está presente antes de realizar el redireccionamiento
+    if (code) {
+      // Realiza cualquier procesamiento adicional necesario con el código de verificación
+      // Por ejemplo, puedes almacenar el código de verificación en una variable de estado o en una cookie
 
-  // Redirecciona al usuario a una página para solicitar el token de acceso
-  router.push(`/request-token?code=${code}`)
+      // Redirecciona al usuario a una página para solicitar el token de acceso
+      router.push(`/request-token?code=${code}`)
+    }
+  }, [code, router])
 
   return null // Opcionalmente, puedes mostrar una página de carga o un mensaje de redirección
 }
