21078e41c7577f2dece40bf6d8d88e961301541b
= <=>
2023-05-30 15:48:15 -0600

    Add files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['multimedia.infojobs.net', 'media.infojobs.net', 'www.infojobs.net'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
